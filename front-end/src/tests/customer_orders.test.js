3a1d8e419e09d912241689e2470da4d20d564b19
valdeci <moura97.valdeci@gmail.com>
2023-03-27 17:33:46 -0300

    tests(orders): update customer orders tests to reflect changes

--- front-end/src/tests/customer_orders.test.js
+++ front-end/src/tests/customer_orders.test.js
@@ -1,7 +1,7 @@
 jest.mock('../utils/api/service');
 
-import React from 'react';
-import { screen, act } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { screen, act, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import CustomerOrders from '../pages/CustomerOrders';
@@ -10,113 +10,119 @@ import * as service from '../utils/api/service';
 import userMock from './mocks/user';
 import orders from './mocks/orders';
 
-const { user, token } = userMock.customer;
-const localStorageCustomer = { ...user, token };
+const { customerLocalStorage } = userMock;
 
 describe('Customer orders page', () => {
   afterEach(() => jest.resetAllMocks());
 
   describe('nav bar', () => {
-    let history;
-
-    beforeEach(() => {
-      localStorage.setItem('user', JSON.stringify(localStorageCustomer));
-      history = renderWithRouter(<CustomerOrders />).history;
-    });
-    
-    afterEach(() => {
-      localStorage.removeItem('user');
-    });
-    
     it('should have the expected elements', () => {
-      const productsLink = screen.getByRole('link', {  name: /produtos/i});
-      const ordersLink = screen.getByRole('link', { name: /pedidos/i});
+      localStorage.setItem('user', JSON.stringify(customerLocalStorage));
+      renderWithRouter(<CustomerOrders />);
+      const productsLink = screen.getByRole('link', { name: /produtos/i });
+      const ordersLink = screen.getByRole('link', { name: /pedidos/i });
       const nameHeading = screen.getByRole('heading', { name: userMock.customer.user.name });
-      const logoutButton = screen.getByRole('button', { name: /sair/i});
+      const logoutButton = screen.getByRole('button', { name: /sair/i });
       
       expect(productsLink).toBeInTheDocument();
       expect(ordersLink).toBeInTheDocument();
       expect(nameHeading).toBeInTheDocument();
       expect(logoutButton).toBeInTheDocument();
+      localStorage.removeItem('user');
     });
 
     describe('link to products', () => {
-      it('should redirect to /customer/products', () => {
+      it('should redirect to /customer/products', async () => {
+        localStorage.setItem('user', JSON.stringify(customerLocalStorage));
+        const { history, user } = renderWithRouter(<CustomerOrders />);
         history.push = jest.fn();
         const productsLink = screen.getByRole('link', { name: /produtos/i});
-        userEvent.click(productsLink);
+        await user.click(productsLink);
 
         expect(history.push).toBeCalledWith({
             hash: '',
             pathname: '/customer/products',
             search: '',
         }, undefined);
+        localStorage.removeItem('user');
       });
     });
 
     describe('link to orders', () => {
-      it('should redirect to /customer/orders', () => {
+      it('should redirect to /customer/orders', async () => {
+        localStorage.setItem('user', JSON.stringify(customerLocalStorage));
+        const { history, user } = renderWithRouter(<CustomerOrders />);
         history.push = jest.fn();
         const ordersLink = screen.getByRole('link', { name: /pedidos/i});
-        userEvent.click(ordersLink);
+        await user.click(ordersLink);
 
         expect(history.push).toBeCalledWith({
             hash: '',
             pathname: '/customer/orders',
             search: '',
         }, undefined);
+        localStorage.removeItem('user');
       });
     });
 
     describe('logout button', () => {
-      it('should clear localStorage', () => {
+      it('should clear localStorage', async () => {
+        localStorage.setItem('user', JSON.stringify(customerLocalStorage));
+        const { user } = renderWithRouter(<CustomerOrders />);
         const logoutButton = screen.getByRole('button', { name: /sair/i});
-        userEvent.click(logoutButton);
+        await user.click(logoutButton);
 
         expect(localStorage.getItem('user')).toBe(null);
         expect(localStorage.getItem('carrinho')).toBe(null);
       });
 
-      it('should redirect to /login', () => {
+      it('should redirect to /login', async () => {
+        localStorage.setItem('user', JSON.stringify(customerLocalStorage));
+        const { history, user } = renderWithRouter(<CustomerOrders />);
         const logoutButton = screen.getByRole('button', { name: /sair/i});
-        userEvent.click(logoutButton);
-
-        const { pathname } = history.location;
-        expect(pathname).toBe('/login');
+        await user.click(logoutButton);
+        expect(history.location.pathname).toBe('/login');
       });
     });
   });
 
   describe('orders', () => {
     describe('if there is any,', () => {
-      let history;
+      let userHistory;
+      let event;
       
       beforeEach(async () => {
         service.customerOrders.mockImplementation(() => Promise.resolve(orders.customerOrders));
-        localStorage.setItem('user', JSON.stringify(localStorageCustomer));
-        await act(async () => {
-          history = renderWithRouter(<CustomerOrders />).history;
-        });
+        localStorage.setItem('user', JSON.stringify(customerLocalStorage));
       });
       
       afterEach(() => {
         localStorage.removeItem('user');
       });
   
-      it('should call service.customerOrders', () => {
+      it('should call service.customerOrders', async () => {
+        await act(async () => {
+          renderWithRouter(<CustomerOrders />);
+        });
         expect(service.customerOrders).toHaveBeenCalledTimes(1);
       });
   
-      it('should call service.customerOrders with user token', () => {
+      it('should call service.customerOrders with user token', async () => {
+        await act(async () => {
+          renderWithRouter(<CustomerOrders />);
+        });
         expect(service.customerOrders)
-        .toHaveBeenCalledWith(localStorageCustomer.token);
+        .toHaveBeenCalledWith(customerLocalStorage.token);
       });
   
-      it('should have the expected elements', () => {
-        const ordersId = screen.getAllByTestId(/customer_orders__element-order-id/i);
-        const ordersStatus = screen.getAllByTestId(/customer_orders__element-delivery-status/i);
-        const ordersDate = screen.getAllByTestId(/customer_orders__element-order-date/i);
-        const ordersPrice = screen.getAllByTestId(/customer_orders__element-card-price/i);
+      it('should have the expected elements', async () => {
+        await act(async () => {
+          renderWithRouter(<CustomerOrders />);
+        });
+        const ordersId = screen.getAllByRole('link', { name: /pedido/gi });
+        const ordersStatus = screen.getAllByText(/pendente/gi);
+        const ordersDate = screen.getAllByText(/.\//gi);
+        const ordersPrice = screen.getAllByText(/r\$/gi);
   
         const ordersElements = [
           ...ordersId,
@@ -134,20 +140,6 @@ describe('Customer orders page', () => {
         expect(ordersDate).toHaveLength(2);
         expect(ordersPrice).toHaveLength(2);
       });
-
-      it('should redirect to /customer/orders/:id when clicking in order card', () => {
-        history.push = jest.fn();
-
-        const ordersId = screen.getAllByTestId(/customer_orders__element-order-id/i);
-
-        userEvent.click(ordersId[0]);
-
-        expect(history.push).toBeCalledWith({
-          hash: '',
-          pathname: `/customer/orders/${orders.sellerOrders[0].id}`,
-          search: '',
-        }, undefined);
-      });
     });
 
     describe('if there is none,', () => {
@@ -155,7 +147,7 @@ describe('Customer orders page', () => {
       
       beforeEach(async () => {
         service.customerOrders.mockImplementation(() => Promise.resolve());
-        localStorage.setItem('user', JSON.stringify(localStorageCustomer));
+        localStorage.setItem('user', JSON.stringify(customerLocalStorage));
         await act(async () => {
           history = renderWithRouter(<CustomerOrders />).history;
         });
@@ -171,7 +163,7 @@ describe('Customer orders page', () => {
   
       it('should call service.customerOrders with user token', () => {
         expect(service.customerOrders)
-        .toHaveBeenCalledWith(localStorageCustomer.token);
+        .toHaveBeenCalledWith(customerLocalStorage.token);
       });
   
       it('should not have orders elements', () => {
