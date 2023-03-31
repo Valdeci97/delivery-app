983fff845560ea709f994d015c6c9b850d7ad697
valdeci <moura97.valdeci@gmail.com>
2023-03-31 10:02:31 -0300

    refactor(forms): create reusable shared password input to forms components

--- front-end/src/components/PasswordInput.jsx
+++ front-end/src/components/PasswordInput.jsx
@@ -1,20 +1,23 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
 
-import * as S from '../styles/common';
-import { RevealPassword, RevealPasswordIcon } from '../styles/login';
 import { handleChange } from '../utils/helpers/handleChange';
+import * as S from '../styles/common';
 import eyeOn from '../assets/eye-icon.svg';
 import eyeOff from '../assets/eye-off-icon.svg';
 
-export default function LoginPasswordInput(props) {
+export default function PasswordInput(props) {
   const [reveal, setReveal] = useState(false);
+
+  const width = props.value.length > 0 ? '80%' : '90%';
 
   return (
     <S.Label htmlFor={ props.static.id }>
       { props.static.title }
       <S.InputContainer>
-        <S.InputIcon src={ props.static.src } alt={ props.static.alt } />
+        <S.InputIconContainer>
+          <S.InputIcon src={ props.static.src } alt={ props.static.alt } />
+        </S.InputIconContainer>
         <S.Input
           data-testid="common_login__input-password"
           id={ props.static.id }
@@ -24,19 +27,20 @@ export default function LoginPasswordInput(props) {
           type={ reveal ? 'text' : 'password' }
           placeholder={ props.static.placeholder }
           value={ props.value }
+          style={ { width } }
         />
         {
           props.value.length > 0
             ? (
-              <RevealPassword
+              <S.Pass
                 type="button"
                 onClick={ () => setReveal(!reveal) }
               >
-                <RevealPasswordIcon
+                <S.PassIcon
                   src={ reveal ? eyeOff : eyeOn }
                   alt={ props.static.alt }
                 />
-              </RevealPassword>
+              </S.Pass>
             ) : null
         }
       </S.InputContainer>
@@ -44,7 +48,7 @@ export default function LoginPasswordInput(props) {
   );
 }
 
-LoginPasswordInput.propTypes = {
+PasswordInput.propTypes = {
   static: PropTypes.shape({
     id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
